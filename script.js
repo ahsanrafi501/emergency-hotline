7786e38e2726e9f200af44fecc8ef9912201c242
Ahsan Habib <habib23105341021@diu.edu.bd>
2025-09-01 15:59:13 +0600

    Call Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const allButtons = document.querySelectorAll('.heart-icon');
+const allHearts = document.querySelectorAll('.heart-icon');
 const countDisplay = document.getElementById('counter');
 
 let count = 0;
@@ -7,7 +7,124 @@ function incrementCounter() {
   count++;
   countDisplay.innerText = count;
 }
-
-for (const button of allButtons) {
-  button.addEventListener('click', incrementCounter);
+for (let heart of allHearts) {
+  heart.addEventListener('click', incrementCounter);
 }
+
+
+
+
+
+
+function callCoin(service, number) {
+  let coin = 20;
+  const availableCoin = parseInt(document.getElementById('coins').innerText);
+  if (availableCoin < coin) {
+    alert('Recharge Your Coin');
+    return;
+  }
+  const newCoin = availableCoin - coin;
+  alert('Service: ' + service + 'Number: ' + 999);
+  document.getElementById('coins').innerText = newCoin;
+
+}
+
+function addHistory(service, number){
+  const availableCoin = parseInt(document.getElementById('coins').innerText);
+  if(counts == count){
+    return;
+  }
+  
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+  const callHistory = document.getElementById('history-added');
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = `
+    <div class="mt-2 bg-slate-300 rounded-lg p-3">
+                    <div class="flex justify-between">
+                        <div class="">
+                            <h2>${service}</h2>
+                            <p>${number}</p>
+                        </div>
+                        <div>
+                            <p>${time}</p>
+
+                        </div>
+                    </div>
+                </div>
+  `
+
+  callHistory.appendChild(newDiv);
+}
+
+
+
+document.getElementById("card1-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'জাতীয় জরুরি সেবা';
+  const number = 999;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+
+document.getElementById("card2-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'পুলিশ';
+  const number = 999;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card3-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'ফায়ার সার্ভিস';
+  const number = 999;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card4-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'অ্যাম্বুলেন্স';
+  const number = 1994999999;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card5-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'নারী ও শিশু সহায়তা';
+  const number = 109;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card6-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'দুদক';
+  const number = 106;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card7-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'বিদ্যুৎ বিভ্রাট';
+  const number = 16216;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card8-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'ব্র্যাক';
+  const number = 16445;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+document.getElementById("card9-call-btn").addEventListener('click', function (e) {
+  e.preventDefault();
+  const services = 'বাংলাদেশ রেলওয়ে';
+  const number = 163;
+  callCoin(services, number);
+  addHistory(services, number);
+})
+
+
+
+
+
